619717c546410c1efbfcf815d13a4bbd678a568e
Ar2rFont <artur.font@estudiantat.upc.edu>
2024-12-20 17:46:38 +0100

    VirtualMouseAdded
    A virtual mouse that moves like the real world mouse has been added here. The sensitivity still needs to be improved to have a more syncronized experience. The Geo of the mouse also needs to be changed

--- Assets/ProjectMOUSE/Scripts/VirtualMouseMaping.cs
+++ Assets/ProjectMOUSE/Scripts/VirtualMouseMaping.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseDotCylinder : MonoBehaviour
+public class VirtualMouseMaping : MonoBehaviour
 {
     [SerializeField]
     private Camera ExternalCamera;  // La càmera des de la qual es veurà el cilindre
@@ -10,9 +10,6 @@ public class MouseDotCylinder : MonoBehaviour
     private GameObject m_targetObject;  // Objecte que es mou amb el raig del ratolí
     [SerializeField]
     private LayerMask raycastLayerMask;  // Mask per limitar les superfícies amb les que interactua el raig
-
-    [SerializeField]
-    private float targetSize = 0.5f; // Tamany desitjat del cilindre en unitats de l'escala de la càmera
 
     void Update()
     {
@@ -25,13 +22,6 @@ public class MouseDotCylinder : MonoBehaviour
             // Obtenir l'objecte amb el qual ha impactat el raig
             GameObject hitObject = hit.collider.gameObject;
             m_targetObject.transform.position = hit.point; // Movem el cilindre al punt d'impacte
-
-            // Calcular la distància entre la càmera i el punt d'impacte
-            float distance = Vector3.Distance(ExternalCamera.transform.position, hit.point);
-
-            // Ajustar l'escala del cilindre perquè sembli de la mateixa mida des de la càmera
-            // Utilitzem la distància per determinar la mida
-            m_targetObject.transform.localScale = new Vector3(targetSize * distance, targetSize * distance, targetSize * distance);
         }
     }
 }
