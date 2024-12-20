8c9a2c5a36405ae42a73fc449dca792beb1879d3
Ar2rFont <artur.font@estudiantat.upc.edu>
2024-12-20 13:00:02 +0100

    4LevelsPossible

--- Assets/ProjectMOUSE/Scripts/ColiderWall.cs
+++ Assets/ProjectMOUSE/Scripts/ColiderWall.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class ColiderWall : MonoBehaviour
 {
     [SerializeField]
-    private string m_tag;
+    private string m_tag1; // Primer tag
+    [SerializeField]
+    private string m_tag2; // Segon tag
+    [SerializeField]
+    private string m_tag3; // Tercer tag
+    [SerializeField]
+    private string m_tag4; // Quart tag
 
     public Material RedMaterial;
     public Material YellowMaterial;
@@ -14,19 +20,43 @@ public class ColiderWall : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.transform.tag == m_tag)
+        if (other.transform.CompareTag(m_tag1))
         {
+            // Si l'objecte té el primer tag, canviem el material a Yellow
             gameObject.GetComponent<Renderer>().material = YellowMaterial;
-            LevelControlScript.StartPuzzle2();
+            LevelControlScript.StartPuzzle1();  // Executa l'acció corresponent al primer tag
+        }
+        else if (other.transform.CompareTag(m_tag2))
+        {
+            // Si l'objecte té el segon tag, realitzem una altra acció (exemple de canvi de color)
+            gameObject.GetComponent<Renderer>().material = RedMaterial;
+            LevelControlScript.StartPuzzle2();  // Executa una acció diferent per al segon tag
+        }
+        else if (other.transform.CompareTag(m_tag3))
+        {
+            // Si l'objecte té el segon tag, realitzem una altra acció (exemple de canvi de color)
+            gameObject.GetComponent<Renderer>().material = RedMaterial;
+            LevelControlScript.StartPuzzle3();  // Executa una acció diferent per al segon tag
+        }
+        else if (other.transform.CompareTag(m_tag4))
+        {
+            // Si l'objecte té el segon tag, realitzem una altra acció (exemple de canvi de color)
+            gameObject.GetComponent<Renderer>().material = RedMaterial;
+            LevelControlScript.StartPuzzle4();  // Executa una acció diferent per al segon tag
         }
     }
+
     private void OnTriggerExit(Collider other)
     {
-        if (other.transform.tag == m_tag)
+        if (other.transform.CompareTag(m_tag1))
         {
+            // Quan un objecte amb m_tag1 surt, restableix el material a Red
+            gameObject.GetComponent<Renderer>().material = RedMaterial;
+        }
+        else if (other.transform.CompareTag(m_tag2))
+        {
+            // Quan un objecte amb m_tag2 surt, restableix el material a Red (o una altra acció)
             gameObject.GetComponent<Renderer>().material = RedMaterial;
         }
     }
-
-
 }
